5813621623a109e88212a638e4004d91d1b78d2c
pedro.costa <pedro.costa@DTCAPTADES01>
2017-02-16 16:59:38 -0200

    Commit files

---
+++ PlejmoTechnicalAssignment/Site/js/unit-tests/MoviesListViewModelTests.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../jquery-3.1.1.js" />
+/// <reference path="jquery.mockjax.js" />
+/// <reference path="../view-models/MoviesListViewModel.js" />
+
+var vm;
+function initTest() {
+    vm = new MoviesListViewModel();
+}
+
+$(function () {
+
+    module("MoviesListViewModel", {
+        setup: initTest
+    });
+
+    test("should have correct number of movies", function () {
+        $.ajax = function (options) {
+            equal(options.url, "../../content/fakedata.json");
+            options.success([{}, {}, {}]);
+        };
+
+        vm.doSomething();
+        equal(vm.movies().length, 3);
+    });
+
+});
---
+++ PlejmoTechnicalAssignment/Site/js/view-models/MoviesListViewModel.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../knockout.js" />
+/// <reference path="../jquery-3.1.1.js" />
+
+var MoviesListViewModel = function () {
+    var self = this;
+    self.movies = ko.observableArray();
+
+    self.doSomething = function () {
+        $.ajax({
+            url: "../../content/fakedata.json",
+            success: function (data) {
+                self.movies = ko.observableArray(data);
+            }
+        })
+    }
+};
